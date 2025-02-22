94df818b3c220e3ee5f038e94b2c1b4979aa8cd5
Aditya Pentyala <adityapentyala02@gmail.com>
2025-02-22 17:15:42 +0530

    calc marginal effects, multiple models

--- regression.r
+++ regression.r
@@ -1,11 +1,30 @@
 library("readxl")
 library("dplyr")
+library("tidyr")
+library("erer")
 
-dataset <- as.data.frame(read_excel("data/processed_data.xlsx"))
+dataset <- as.data.frame(read_excel("data/all_processed_data.xlsx"))
 dataset <- drop_na(dataset)
 
-head(dataset)
+formula_1 <- Bankrupt. ~ Total.debt.Total.net.worth + Debt.ratio.. + 
+                    Borrowing.dependency + Degree.of.Financial.Leverage..DFL.
 
-probit_model <- glm(Bankrupt. ~ Total.debt.Total.net.worth + Debt.ratio.. + 
-                    Borrowing.dependency + Degree.of.Financial.Leverage..DFL.,
+formula_2 <- Bankrupt. ~ Total.debt.Total.net.worth + Debt.ratio.. + 
+                    Borrowing.dependency + Degree.of.Financial.Leverage..DFL. +
+                    Operating.Profit.Per.Share..Yuan...+ ROA.C+ # nolint: line_length_linter.
+                    Current.Ratio
+
+formula_3 <- Bankrupt. ~ Total.debt.Total.net.worth + Debt.ratio.. + 
+                    Borrowing.dependency + Degree.of.Financial.Leverage..DFL. +
+                    Operating.Profit.Per.Share..Yuan...+ ROA.C+ # nolint: line_length_linter.
+                    Current.Ratio + Total.Asset.Turnover + Working.Capital.to.Total.Assets
+
+
+probit_model_base <- glm(formula_1, # nolint
                     data=dataset, family=binomial(link="probit"), x=TRUE)
+
+probit_model_altman <- glm(formula_3, # nolint
+                    data=dataset, family=binomial(link="probit"), x=TRUE)
+
+me_base <- maBina(probit_model_base, x.mean = TRUE, rev.dum = TRUE, digits = 3, subset.name = NULL, subset.value)
+me_altman <- maBina(probit_model_altman, x.mean = TRUE, rev.dum = TRUE, digits = 3, subset.name = NULL, subset.value)
--- utils.r
+++ utils.r
@@ -2,5 +2,6 @@ library("readxl")
 library("writexl")
 
 raw_data <- read.csv("data/data.csv", header=TRUE)
-subset_data <- raw_data[, c(1, 37, 38, 41, 93)]
-write_xlsx(raw_data, "./data/processed_data.xlsx", col_names=TRUE)
+subset_data <- raw_data[, c(1, 2, 23, 34, 37, 38, 41, 93)]
+write_xlsx(raw_data, "./data/all_processed_data.xlsx", col_names=TRUE)
+write_xlsx(subset_data, "./data/processed_data.xlsx", col_names=TRUE)
